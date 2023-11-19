87bb43d8eae35969b2bfe15057898aeffd5d904f
moon-ic <3276817499@qq.com>
2023-11-19 11:01:56 +0800

    fix: package配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './route';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
 	<>{router ? <RouterProvider router={router} /> : null}</>,
 );
